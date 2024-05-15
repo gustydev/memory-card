8bfa8407bdd9a23191bcefeba4ec8d4ca17ff2e8
gusty <gustavodiorlando@gmail.com>
2024-05-15 12:29:17 -0300

    Add effects for card creation and icon fetching

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,14 +10,14 @@ function Card({ cardUrl, cardTitle }) {
 }
 
 export default function Game( {currentScore, setCurrentScore, topScore, setTopScore} ) {
-    const [cardList, setCards] = useState([]);
+    const [cards, setCards] = useState([]);
     const [mobData, setData] = useState(null);
 
     useEffect(() => {
         let ignore = false;
 
         async function fetchData() {
-            const response = await fetch('https://maplestory.io/api/gms/28/mob?count=22');
+            const response = await fetch('https://maplestory.io/api/gms/28/mob?count=34');
             const data = await response.json();
             if (!ignore) {
                 setData(data);
@@ -30,9 +30,46 @@ export default function Game( {currentScore, setCurrentScore, topScore, setTopSc
         }
     }, [])
 
-    console.log(mobData);
+    useEffect(() => {
+        let newCards = [];
+        if (mobData) {
+            while (newCards.length < 10) {
+                const random = Math.floor(Math.random() * mobData.length);
+                if (!newCards.find((c) => c.name === mobData[random].name)) {
+                    newCards.push({name: mobData[random].name, id: mobData[random].id})
+                }
+            }
+            setCards(newCards);
+        }
+    }, [mobData])
+
+    useEffect(() => {
+        let ignore = false;
+
+        async function fetchIcon(id) {
+            const response = await fetch(`https://maplestory.io/api/gms/28/mob/${id}/icon`);
+            const icon = response.url;
+            return icon;
+        }
+
+        const newCards = cards;
+
+        if (newCards.length === 10) {
+            newCards.forEach(async (card) => {
+                const icon = await fetchIcon(card.id);
+                card.icon = icon;
+            })
+        }
+        if (!ignore) {
+            setCards(newCards);
+        }
+
+        return () => {
+            ignore = true;
+        }
+    }, [cards])
 
     return (
-        <div>lul</div>
+        cards ? <div>yay</div> : 'error fetching api'
     )
 }
